b9fcd015f23ce7500ab9c82d6ec9efe6d0019dda
JamieBurridge <jjb98b@gmail.com>
2022-10-11 10:43:38 +0100

    User can input specific column names

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ column_types = click.Choice(
         'name',
         'date',
         'email',
+        'text',
         'address',
         'zipcode',
     ]
@@ -23,16 +24,16 @@ def dummy_writer():
 
 @dummy_writer.command
 @click.option('--table', '-t','table', type=click.STRING, help='Table name')
-@click.option('--column', '-c','columns', multiple=True, type=column_types, help='Table column Value')
+@click.option('--column', '-c','columns', multiple=True, type=click.STRING, help='Column name')
+@click.option('--columnValue', '-v','columnValues', multiple=True, type=column_types, help='Table column value')
 @click.option('--records', '-n','records', type=click.types.INT, default=50, show_default=True, prompt=True, help='Number of records to create')
-def go(table:str, columns:list, records:int): 
+def go(table:str, columns:list, columnValues:list, records:int): 
     table = Table(name=str(table))
     rows = []
     for _ in range(records): 
         rows.append( tuple(
-            getattr(fake,column)()
-            for column in columns if hasattr(fake,column)
+            getattr(fake,columnValue)()
+            for columnValue in columnValues if hasattr(fake,columnValue)
         ))
-    click.echo(str(Query.into(table).insert(rows)))
+    click.echo(str(Query.into(table).columns(columns).insert(rows)))
             
-        
